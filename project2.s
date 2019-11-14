14554d6b2d318e887ed365e54afee93214f95d70
Emmanuel Fowora <43800038+Fowora@users.noreply.github.com>
2019-11-13 21:59:30 -0500

    Taking in user input

--- project2.s
+++ project2.s
@@ -1,1 +1,13 @@
 # Emmanuel Fowora
+.data #Data declaration sector
+my_string: .space 2000
+invalid_input: .asciiz "This input is invalid"
+
+.text
+
+main:
+	li $v0, 8 #takes in user input
+	la $a0, my_string
+	syscall
+	
+	
