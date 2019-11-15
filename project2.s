d927c1828e158adaf2a82c9d58f4bd0d5400dfa8
Emmanuel Fowora <43800038+Fowora@users.noreply.github.com>
2019-11-14 22:37:14 -0500

    Check for spaces at the beginning of the string

--- project2.s
+++ project2.s
@@ -1,13 +1,23 @@
 # Emmanuel Fowora
-.data #Data declaration sector
-my_string: .space 2000
-invalid_input: .asciiz "This input is invalid"
 
+.data
+	data: .space 1001
+	output: .asciiz "\n"
+	invalid_input: .asciiz "Invalid input"
 .text
 
 main:
-	li $v0, 8 #takes in user input
-	la $a0, my_string
+	#storing user input
+	li $v0, 8
+	la $a0, data
+	li $a1, 1001
 	syscall
-	
-	
+	jal beginning
+	j finish
+
+beginning:
+	#checks for spaces at the beginning of the string
+	la $t0,data
+	add $t0,$t0,$t1
+	lb $s0, ($t0)
+	beq $s0, 0, end		
