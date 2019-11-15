22bb8feb84a3ae235d5a0fadfb2fe66309059a14
Emmanuel Fowora <43800038+Fowora@users.noreply.github.com>
2019-11-14 23:42:27 -0500

    Checks the rest of the characters

--- project2.s
+++ project2.s
@@ -20,4 +20,23 @@ beginning:
 	la $t0,data
 	add $t0,$t0,$t1
 	lb $s0, ($t0)
-	beq $s0, 0, end		
+	beq $s0, 0, end	
+		beq $s0, 9, pass
+	beq $s0, 32, pass
+	move $t6, $t1
+	j while
+
+pass:
+	#passes by the spaces in the string
+	addi $t1, $t1,1
+	j beginning	
+	
+while:
+	#checks the rest of the characters after passing the first character
+    li $t7, -1
+    la $t0,data
+    add $t0,$t0,$t1
+    lb $s0, ($t0)
+    bge $t2, 5, not_valid
+    bge $t3, 1, not_valid
+    j check_characters	
